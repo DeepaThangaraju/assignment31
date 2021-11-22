bda56959ab6b34d0d83c4ead12f544003f18a5d0
DeepaThangaraju <deepat2405@gmail.com>
2021-11-21 22:36:12 -0800

    back button

--- src/Vechicaldetails.js
+++ src/Vechicaldetails.js
@@ -1,9 +1,11 @@
 import { useParams } from 'react-router';
 import { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export function Vechicaldetails() {
     const { id } = useParams();
     const [vehical, setVehical] = useState({});
+    const history = useHistory();
     useEffect(() => {
         fetch(`https://6166c4e813aa1d00170a6717.mockapi.io/vechicles/${id}`,
             { method: "GET" })
@@ -16,6 +18,7 @@ export function Vechicaldetails() {
             <p>Model Name:{vehical.name}</p>
             <p>Rate:Rs.{vehical.rate}Lakhs</p>
             <p>Milage:{vehical.milage}</p>
+            <button onClick={() => history.goBack()}>BACK</button>
         </div>
     );
 }
